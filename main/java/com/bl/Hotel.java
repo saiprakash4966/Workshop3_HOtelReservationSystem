e32e752d617d40f6f12c281bdebd0fe499c2c095
Your Name <you@example.com>
2022-03-19 09:17:57 +0530

    Welcome Message

---
+++ main/java/com/bl/Hotel.java
@@ -0,0 +1,10 @@
+package com.bl;
+
+public class Hotel
+{
+	public static void main(String[]args)
+	{
+		System.out.println("Welcome to Hotel Resevation System");
+	}
+
+}
---
+++ test/java/com/bl/HotelTest.java
@@ -0,0 +1,6 @@
+package com.bl;
+
+public class HotelTest
+{
+	
+}
